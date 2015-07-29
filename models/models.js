eee03e0ba04d6ecd560e8b2f23cab1767c172a3d
Carlos <garciagarciacarlos@gmail.com>
2015-07-29 22:51:34 +0200

    Añadido soporte para añadir comentarios a las preguntas

--- models/models.js
+++ models/models.js
@@ -21,7 +21,14 @@ var sequelize = new Sequelize(DB_name, user, pwd, {dialect: protocol, protocol: 
 //Importar la definición de la tabla Quiz en quiz.js
 var Quiz = sequelize.import(path.join(__dirname, 'quiz'));
 
+//Importar la definición de la tabla Quiz en quiz.js
+var Comment = sequelize.import(path.join(__dirname, 'comment'));
+
+Comment.belongsTo(Quiz);
+Quiz.hasMany(Comment);
+
 exports.Quiz = Quiz; //exportar definición de la tabla Quiz
+exports.Comment = Comment;
 
 //sequelize.sync() crea e inicializar tabla de pregunta en DB
 sequelize.sync().then(function(){
